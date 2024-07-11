ebcf3788fc9d30784c37589f78656c47e7550c41
Aberhouch-Abdessamad$  git config --global user.name Aberhouch-Abdessamad <aberabdess@gmail.com>
2024-07-11 12:50:16 +0100

    update

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <View />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  },
 });
---
+++ Components/View.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+
+function View  () {
+  return (
+    <View style={styles.container}>
+      
+      <Image 
+        source={require('./assets/profile (1).gif')} 
+        style={[styles.playerIcon, styles.topIcon]} 
+      />
+      
+     
+      <Image 
+        source={require('./assets/ronda_maroc.png')} 
+        style={styles.image} 
+      />
+
+      
+      <Image 
+        source={require('./assets/profile.gif')} 
+        style={[styles.playerIcon, styles.bottomIcon]} 
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    position: 'relative',
+  },
+  image: {
+    width: 300,
+    height: 400,
+    resizeMode: 'contain',
+  },
+  playerIcon: {
+    position: 'absolute',
+    width: 50,
+    height: 50,
+    resizeMode: 'contain',
+  },
+  topIcon: {
+    top: 10, 
+    left: 10, 
+  },
+  bottomIcon: {
+    bottom: 10, 
+    right: 10, 
+  },
+});
+
+export default View;
